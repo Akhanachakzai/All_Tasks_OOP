9455b0d7ea350e4d2baf833d07ff7af7430bb558
Akhanachakzai <akhanachakzai1@gmail.com>
2015-12-01 13:11:57 +0500

    all tasks

---
+++ Clock/src/clock/Main.java
@@ -0,0 +1,18 @@
+package clock;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Alarmclock obj = new Alarmclock();
+		obj.setAlarm(0, 16);
+		for (int x=0;x<=1000;x++){
+			obj.tick();
+			System.out.print(obj.viewHr()+":"+obj.viewMin());
+			System.out.println(":"+obj.viewSec());
+			System.out.flush();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
